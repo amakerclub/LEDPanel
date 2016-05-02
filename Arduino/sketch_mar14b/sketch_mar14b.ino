f16b609b6ca0406c8b325070e57a5532de7b0dcd
Thierry ACCART <thierry.accart@amadeus.com>
2016-05-02 13:38:13 +0200

    Update code for message scroll - non functional

--- Arduino/sketch_mar14b/sketch_mar14b.ino
+++ Arduino/sketch_mar14b/sketch_mar14b.ino
@@ -4,106 +4,109 @@
 
 
 /*Bitmap of ANSI chars - letters+numbers+common signs:  starts at char Ox21, ends at 0x7E */
-const char[] ansiCharsByRows = ("1818181818001800",
- "6c6c6c0000000000",
- "36367f367f363600",
- "0c3f683e0b7e1800",
- "60660c1830660600",
- "386c6c386d663b00",
- "0c18300000000000",
- "0c18303030180c00",
- "30180c0c0c183000",
- "00187e3c7e180000",
- "0018187e18180000",
- "0000000000181830",
- "0000007e00000000",
- "0000000000181800",
- "00060c1830600000",
- "3c666e7e76663c00",
- "1838181818187e00",
- "3c66060c18307e00",
- "3c66061c06663c00",
- "0c1c3c6c7e0c0c00",
- "7e607c0606663c00",
- "1c30607c66663c00",
- "7e060c1830303000",
- "3c66663c66663c00",
- "3c66663e060c3800",
- "0000181800181800",
- "0000181800181830",
- "0c18306030180c00",
- "00007e007e000000",
- "30180c060c183000",
- "3c660c1818001800",
- "3c666e6a6e603c00",
- "3c66667e66666600",
- "7c66667c66667c00",
- "3c66606060663c00",
- "786c6666666c7800",
- "7e60607c60607e00",
- "7e60607c60606000",
- "3c66606e66663c00",
- "6666667e66666600",
- "7e18181818187e00",
- "3e0c0c0c0c6c3800",
- "666c7870786c6600",
- "6060606060607e00",
- "63777f6b6b636300",
- "6666767e6e666600",
- "3c66666666663c00",
- "7c66667c60606000",
- "3c6666666a6c3600",
- "7c66667c6c666600",
- "3c66603c06663c00",
- "7e18181818181800",
- "6666666666663c00",
- "66666666663c1800",
- "63636b6b7f776300",
- "66663c183c666600",
- "6666663c18181800",
- "7e060c1830607e00",
- "7c60606060607c00",
- "006030180c060000",
- "3e06060606063e00",
- "183c664200000000",
- "00000000000000ff",
- "1c36307c30307e00",
- "00003c063e663e00",
- "60607c6666667c00",
- "00003c6660663c00",
- "06063e6666663e00",
- "00003c667e603c00",
- "1c30307c30303000",
- "00003e66663e063c",
- "60607c6666666600",
- "1800381818183c00",
- "1800381818181870",
- "6060666c786c6600",
- "3818181818183c00",
- "0000367f6b6b6300",
- "00007c6666666600",
- "00003c6666663c00",
- "00007c66667c6060",
- "00003e66663e0607",
- "00006c7660606000",
- "00003e603c067c00",
- "30307c3030301c00",
- "0000666666663e00",
- "00006666663c1800",
- "0000636b6b7f3600",
- "0000663c183c6600",
- "00006666663e063c",
- "00007e0c18307e00",
- "0c18187018180c00",
- "1818180018181800",
- "3018180e18183000",
- "316b460000000000") ; 
+ String  ansiCharsByCols [97] = {"0000000000000000",
+  "000000fa00000000",
+  "0000000000000000",
+  "0000c000c0000000",
+  "000028fe28fe2800",
+  "00002454fe544800",
+  "00c4c810204c8c00",
+  "000c72926a040a00",
+  "0000002040800000",
+  "0000003844820000",
+  "0000824438000000",
+  "000028107c102800",
+  "000010107c101000",
+  "0000020c0c000000",
+  "0000101010101000",
+  "0000000c0c000000",
+  "0000040810204000",
+  "00007c8a92a27c00",
+  "00000042fe020000",
+  "0000468a8a926200",
+  "00008482a2d28c00",
+  "0000182848fe0800",
+  "0000e4a2a2a29c00",
+  "00003c5292920c00",
+  "0000808e90a0c000",
+  "00006c9292926c00",
+  "0000609292947800",
+  "0000006c6c000000",
+  "0000026c6c000000",
+  "0000102844820000",
+  "0000282828282800",
+  "0000824428100000",
+  "0040809a90600000",
+  "00007c82baaa7a00",
+  "00007e9090907e00",
+  "000082fe92926c00",
+  "00007c8282824400",
+  "000082fe82827c00",
+  "0000fe9292828200",
+  "0000fe9090808000",
+  "00007c828a8a4e00",
+  "0000fe101010fe00",
+  "00000082fe820000",
+  "0000040282fc8000",
+  "0000fe1028448200",
+  "0000fe0202020200",
+  "0000fe403040fe00",
+  "0000fe201008fe00",
+  "00007c8282827c00",
+  "0000fe9090906000",
+  "00007c828a847a00",
+  "0000fe9098946200",
+  "000044a2928a4400",
+  "00008080fe808000",
+  "0000fc020202fc00",
+  "0000f8040204f800",
+  "0000fc021c02fc00",
+  "0000c6281028c600",
+  "0000e0100e10e000",
+  "0000868a92a2c200",
+  "00003cc242c22400",
+  "00003c4242c22400",
+  "000092fe92827c00",
+  "000024d24aca2400",
+  "000042c64ad26200",
+  "0000204080402000",
+  "00000854543c0000",
+  "0000fc2424180000",
+  "0000384444440000",
+  "0000182424fc0000",
+  "0000385454200000",
+  "0000107e90400000",
+  "0000304a4a7c0000",
+  "0000fc20201c0000",
+  "00000024bc040000",
+  "00000422bc000000",
+  "0000fc1028040000",
+  "00000084fc040000",
+  "00007c403c403c00",
+  "00007c40403c0000",
+  "0000384444380000",
+  "00007e4848300000",
+  "0030484a7e020200",
+  "00007c2040400000",
+  "0000245454480000",
+  "00000040f8440000",
+  "00007804047c0000",
+  "0000700804087000",
+  "0000780418047800",
+  "0000442810284400",
+  "0000700a0a7c0000",
+  "0000444c54640000",
+  "000018a4a4a40000",
+  "0000182424a40000",
+  "0000081454fc4000",
+  "000024d454c80000",
+  "000024acb4a40000"};
 
+  
 #define pixelCount 64
 //amaker 
-//#define PANELPIN 4
-//perso
 #define PANELPIN 4
+
 #define LEDPIN 2
 const char WiFiAPPSK[] = "aMakerESP8266";
 
@@ -183,24 +186,66 @@ void started(){
  }
 
 String getCharHexa(char value) {
-  if (value >= 20) && (value <= 0x7e) {
-    return ansiCharsByRows[value - 20];
+  if (value >= 20 && value <= 0x7e) {
+    return ansiCharsByCols[value - 20];
   }
   else {
     return "ffffffffffffffff";
   }
 }
-void printScroll(String input, int delay) {
+void handleHexaStringRequest(String req, int waitms) {
+  printDebug("req="+req);
+
+  RgbColor white = RgbColor(128,128,128);
+  RgbColor black = RgbColor(0,0,0);
+  int off = 0;
+  int pixelNumber= 0;
+  for (int curCol = 0; curCol < 8; ++curCol) {
+    int colValue = hexaToInt(req.charAt(2*curCol)) << 8 + hexaToInt(req.charAt(2*curCol + 1));
+    int checker;
+    if (curCol % 2 == 0) {
+      checker = 1;
+    }
+    else {
+      checker = 128;
+    }
+    
+    for (int curBit = 0; curBit < 8; curBit++) {
+      if ((curCol & checker) == checker) {
+        strip.SetPixelColor(pixelNumber++, white);      
+      }
+      else {
+        strip.SetPixelColor(pixelNumber++, black);            
+      }
+      if (curCol % 2 == 0) {
+        checker << 1;
+      }
+      else {
+        checker >> 1;
+      }      
+    }
+  }
+  
+  strip.Show();
+  delay(waitms);       
+}
+
+
+
+
+void printScroll(String input, int waitms) {
   // Need to add a space at the beginning/ end of the string
   int curOffset_message = 0;
   while (true) {
-    String Chars_2 = getCharHexa[input.at(currOffset_message)] + getCharHexa(input.at((currOffset_message + 1) % length(input)));
+    
+    String Chars_2 =   getCharHexa(input.charAt(curOffset_message)) +  getCharHexa(input.charAt((curOffset_message + 1) % input.length()));
+    
     for (int offsetLetter = 0; offsetLetter < 8; ++offsetLetter) {
+      handleHexaStringRequest(Chars_2 , waitms);
       
-      sleep(delay);       
     }
     curOffset_message = curOffset_message + 1;
-    if (curOffset_message > length(input)) {
+    if (curOffset_message > input.length()) {
       curOffset_message = 0;
     }
   }
@@ -263,40 +308,6 @@ void handleSetRequest(String req ) {
       } 
       strip.Show();
     }
-}
-
-void handleHexaStringRequest(String req) {
-  printDebug("req="+req);
-
-  Color white = RgbColor(128,128,128);
-  Color black = RgbColor(0,0,0);
-  int off = 0;
-  for (int curCol = 0; curCol < 8; ++curCol) {
-    int colValue = hexaToInt(req.at(2*curCol)) << 8 + hexaToInt(req.at(2*curcol + 1));
-    int checker;
-    if (curCol % 2 == 0) {
-      checker = 1;
-    }
-    else {
-      checker = 128;
-    }
-    for (int curBit = 0; curBit < 8; curBit++) {
-      if ((curCol & checker) == checker) {
-        strip.SetPixelColor(pixelNumber++, white);      
-      }
-      else {
-        strip.SetPixelColor(pixelNumber++, black);            
-      }
-      if (curCol % 2 == 0) {
-        checker << 1;
-      }
-      else {
-        checker >> 1;
-      }      
-    }
-  }
-  
-  strip.Show();
 }
 
 /* REST service to set pixel by x.y=RRGGBB;x.y=RRGGBB;x.y=RRGGBB;.. */
@@ -362,6 +373,24 @@ void handleDoRequest(String req ){
       strip.Show();
     }
 }
+/* REST service to set entire image colors=RRGGBB,RRGGBB,RRGGBB,RRGGBB,....,RRGGBB (64 colors passed */
+void handleMessageScrollRequest(String req ){
+    blink(1);
+
+    printDebug("Confirm handleMessageScrollRequest  received");
+    printDebug(req);
+    
+    size_t equals = req.indexOf("?");
+    if (equals != -1) {
+      String message=req.substring(equals+1);
+      printDebug("message : " + message);
+      
+      
+      strip.Show();
+    }
+}
+
+
 void handleDefault(WiFiClient client){
 blink(1);
 
@@ -411,6 +440,9 @@ void loop()
   
   if (req.indexOf("/do?") != -1) {
     handleDoRequest(req);
+  }
+  if (req.indexOf("/msg?") != -1) {
+    handleMessageScrollRequest(req);
   }
   if (req.indexOf("/set?") != -1) {
     handleSetPixelRequest((char*)req.c_str());
